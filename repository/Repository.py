51c4885f792902c33ed42060ad802d67cf49cdd1
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-08-09 23:09:57 -0300

    Ajusts

--- repository/Repository.py
+++ repository/Repository.py
@@ -14,6 +14,20 @@ class GenericRepository(Interceptor):
             user="postgres",
             password="postgres")
 
+    def get_fii(self, fii):
+        keys = ['last_update', 'desv_dy', 'segment']
+        ks = str(keys).replace("[", "").replace("]", "").replace("'", "")
+        select_ = f"select {ks} from fiis where ticker='{fii['ticker']}'"
+        cursor, cursor_ = self.execute_select(select_)
+        for row in cursor_:
+            i = 0
+            for key in keys:
+                fii[key] = row[i]
+                i += 1
+        cursor.close()
+        return fii
+
+
     def execute_select(self, select_):
         cursor = self.conn.cursor()
         cursor.execute(select_)
