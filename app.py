3a0df7f9a11594e4271d109268d11e2274c2f628
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-08-12 18:55:54 -0300

    Improvemts in investments

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def att_all():  # put application's code here
 
 
 @app.route('/att-express', methods=['POST'])
-def att_all():  # put application's code here
+def att_express():  # put application's code here
     att_stocks.att_expres()
     return "{}", 200, {'Content-Type': 'application/json'}
 
