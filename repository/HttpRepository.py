595c4395ad82985cb599b9b6e419dd0a2f42e1a2
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-11-12 22:14:21 -0300

    Pass by bff

--- repository/HttpRepository.py
+++ repository/HttpRepository.py
@@ -11,7 +11,7 @@ headers_sti = {
     'User-Agent': 'PostmanRuntime/7.26.8'
 }
 
-url_repository = 'http://repository:5000/'
+url_repository = 'http://bff:8080/repository/'
 
 
 class HttpRepository(Interceptor):
