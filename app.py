0443f5c4f95a621386b2e085904794d6ad435bc1
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2023-12-09 17:00:56 -0300

    Enable force option and working hours check
    The commit allows the 'att_express' endpoint to only run during working hours by default. It also introduces a 'force' option in the request headers, enabling the endpoint to be triggered outside working hours if necessary. This helps to control resource usage and prevent potential issues by running during appropriate times.

--- app.py
+++ app.py
@@ -299,9 +299,10 @@ def att_prices_thr(headers):
 
 @app.route('/att-express', methods=['POST'])
 def att_express():
-    print("att_express requested")
-    headers = request.headers
-    threading.Thread(target=att_stocks.att_expres, args=(headers,)).start()
+    if (utils.work_day() and utils.work_time()) or request.headers.get('force') == 'true':
+        print("att_express requested")
+        headers = request.headers
+        threading.Thread(target=att_stocks.att_expres, args=(headers,)).start()
     return "{}", 200, {'Content-Type': 'application/json'}
 
 
